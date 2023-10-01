cace231b319d556c734aff7b8b98c84112f30ed5
Adham Magdy <adhammagdy614@gmail.com>
2023-10-01 16:32:16 +0300

    validating the budget

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -11,6 +11,13 @@ const {
 // Define routes
 router.post('/signup', validateUser, createNewUser);
 router.post('/login', validateUser, userLogin);
-router.put('/budget', authenticateUser, updateBudget);
+// Update user's current budget
+router.put(
+  '/budget',
+  authenticateUser,
+  validateUser, // Apply user validation rules
+  validateRequest([body('currentBudget').custom(validateBudget)]), // Validate currentBudget
+  updateBudget
+);
 
 module.exports = router;
