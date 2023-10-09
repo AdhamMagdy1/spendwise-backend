77a14e13eaf26276d2eb06b1e520ac0457b50068
Adham Magdy <adhammagdy614@gmail.com>
2023-10-09 20:20:24 +0300

    changed the server port

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use('/api/spending', spendingRoutes); // Spending-related routes
 app.use(errorHandler); // Use the error handling middleware at the end of your middleware stack
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
