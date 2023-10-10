7141eff759f4c5c1f9e3d1532bd3d8638d24b047
Adham Magdy <adhammagdy614@gmail.com>
2023-10-10 13:51:38 +0300

    edited the serve host

--- server.js
+++ server.js
@@ -34,6 +34,6 @@ app.use(errorHandler); // Use the error handling middleware at the end of your m
 
 // Start the server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, "192.168.1.5" || "localhost", () => {
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
