6b81d1f0896940f23c3297c03f13dd23faccb9da
Adham Magdy <adhammagdy614@gmail.com>
2023-10-12 16:01:01 +0300

    added port to env

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use('/api/spending', spendingRoutes); // Spending-related routes
 app.use(errorHandler); // Use the error handling middleware at the end of your middleware stack
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
